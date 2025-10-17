f2b0105a05e710cbb7658e2d3f648b972966e749
beeboboboboe <125676148+beeboboboboe@users.noreply.github.com>
2025-10-17 16:34:24 +0000

    w

--- VRPractical2_2/script.js
+++ VRPractical2_2/script.js
@@ -1,5 +1,43 @@
 
-window.addEventListener("DOMContentLoaded",function() {
+let scene;
+let sphere;
+let entity
 
-    
+window.addEventListener("DOMContentLoaded",function() {
+scene=document.querySelector("a-scene");
+car=document.querySelector("#car");
+pokeball=document.querySelector("#pokemonball");
+rocket=document.querySelector("#rocket");
+dude=document.querySelector("#dude");
+sun=document.querySelector("#sun");
+car.x=0;
+car.dx=.01;
+pokeball.x=1;
+pokeball.dx=.1;
+rocket.y=1;
+rocket.dy=.01;
+dude.s=0;
+dude.ds=.01
+
+loop()
 })
+
+
+function loop(){
+car.x+=car.dx;
+car.setAttribute("position", {x:car.x, y:0, z:-6});
+pokeball.x+=pokeball.dx;
+pokeball.setAttribute("rotation", {x:pokeball.x, y:0, z:-5});
+rocket.y+=rocket.dy;
+rocket.setAttribute("position", {x:4, y:rocket.y, z:-4});
+dude.s+=dude.ds;
+dude.setAttribute("scale", {})
+
+
+
+
+window.requestAnimationFrame(loop);
+
+}
+
+
