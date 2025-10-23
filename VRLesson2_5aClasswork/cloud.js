010cfbc253de60c470e7e637042afa89b243c5ac
beeboboboboe <125676148+beeboboboboe@users.noreply.github.com>
2025-10-23 16:34:18 +0000

    started 2_5a

--- VRLesson2_5aClasswork/cloud.js
+++ VRLesson2_5aClasswork/cloud.js
@@ -1,6 +1,11 @@
 class Cloud{
-  constructor(x,y,z){
+  constructor(y,z){
     // Challenge 1: Create animation variables  x and dx for the Cloud to fly.  Set them to appropriate values.
+    this.x=x;
+    this.y=y;
+    this.z= z;
+    this.x=0;
+    this.dx=.001;
     this.obj = document.createElement("a-entity");
     let shapes = ["a-dodecahedron","a-icosahedron","a-octahedron","a-tetrahedron"];
 
@@ -14,4 +19,10 @@ class Cloud{
     scene.append( this.obj )
   }
   //Challenge 2: Create a function fly() which increases x by dx and updates the Cloud object to that x position
+ 
+fly(){
+this.x+=this.dx;
+this.obj.setAttribute("position", {x:0, y:this.p, z:0});
+}
+
 }
