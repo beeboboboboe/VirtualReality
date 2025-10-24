479e517ebea4b3ca39e63eede16aee5e8ead9a37
beeboboboboe <125676148+beeboboboboe@users.noreply.github.com>
2025-10-24 18:15:34 +0000

    finished claasswork

--- VRLesson2_5bClasswork/script.js
+++ VRLesson2_5bClasswork/script.js
@@ -22,7 +22,7 @@ window.addEventListener("DOMContentLoaded",function() {
   cloud = new Cloud(-5,10,-1);
  let clouds=[];
  window.addEventListener("DOMContentLoaded", function(){
-for(let i=0; i<50; i++){
+for(let i=0; i<20; i++){
 x=rnd(-20, 20);
 z=rnd(-20, 20);
 let c = new Cloud(x, 20, z);
@@ -35,6 +35,18 @@ clouds.push(c)
  )
   //Challenge 3: Create an array to store 100 snowflakes in random positions
 
+snowflake = new Snowflake(0, 9, 0);
+let snowflakes=[];
+window.addEventListener("DOMContentLoaded", function*(){
+for(let i=0; i<100; i++){
+x=rnd(-20, 20);
+z=rnd(-20, 20);
+let s = new Snowflake(x, 9, z);
+snowflakes.push(s);
+}
+
+})
+
   loop();
 })
 
@@ -44,7 +56,9 @@ function loop(){
   }
   cloud.fly();
   //Challenge 2: Traverse your array of clouds and make each cloud fly
-
+for(c of clouds){
+  c.fly()
+}
   //Challenge 4: Traverse your array of snowflakes and make each snowflake fall
   
   window.requestAnimationFrame( loop );
