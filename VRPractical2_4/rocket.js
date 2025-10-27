17226c603273378712f14a0417871c0153fccf27
beeboboboboe <125676148+beeboboboboe@users.noreply.github.com>
2025-10-27 16:34:12 +0000

    working

---
+++ VRPractical2_4/rocket.js
@@ -0,0 +1,38 @@
+class Rocket{
+ constructor(x,y,z){
+     this.x = x;
+    this.y = y;
+    this.z = z;
+    this.y=-4
+    this.dy = 0.03;
+
+    
+ this.obj = document.createElement("a-entity");
+ 
+ let base = document.createElement("a-cylinder");
+ base.setAttribute("radius", "1");
+ base.setAttribute("color", "#808080");
+ base.setAttribute("height", "4");
+this.obj.append( base );
+
+let tip = document.createElement("a-cone");
+tip.setAttribute("radius", "1");
+tip.setAttribute("color", "red");
+tip.setAttribute("position", "0, 2.5, 0")
+this.obj.append( tip );
+
+
+scene.append( this.obj );
+this.obj.setAttribute("position", {x:0, y:-4, z:0});
+
+
+ }
+ 
+launch(){
+this.y+=this.dy;
+this.obj.setAttribute("position",{x:0, y:this.y, z:0});
+
+}
+
+ 
+}
--- VRPractical2_4/script.js
+++ VRPractical2_4/script.js
@@ -1,15 +1,25 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
-let scene;
+let scene, rocket;
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene"); 
-
-
+  rocket = new Rocket(0, -4, 0);
+let rockets=[];
+window.addEventListener("DOMContentLoaded", function*(){
+for(let i=0; i<100; i++){
+x=rnd(-20, 20);
+z=rnd(-20, 20);
+let r = new Rocket(x, -4, z);
+rockets.push(r);
+}
   loop();
 })
 
-function loop(){
 
+
+
+function loop(){
+rocket.launch()
   
   window.requestAnimationFrame( loop );
 }
