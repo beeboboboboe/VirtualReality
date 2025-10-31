18abf653a96862f01b473137a8974f72ea5311e6
beeboboboboe <125676148+beeboboboboe@users.noreply.github.com>
2025-10-31 16:35:15 +0000

    work

--- VRLesson2_7Classwork/script.js
+++ VRLesson2_7Classwork/script.js
@@ -1,16 +1,25 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
 let scene, dudeTemplate;
 
+dudes=[];
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   dudeTemplate = document.querySelector("#dudeTemplate");
 
   //Challenge 4: Create an array of dudes at random locations.
-  
+  for(let i=0;i<20;i++){
+    x=rnd(-20, 20)
+    z=rnd(-20, 20)
+    let d = new Dude(x, 1, z);
+    dudes.push(d);
+  }
   loop();  
 })
 function loop(){
   //Challenge 5: Make all the dudes jump.
-  
+  for(d of dudes){
+    
+
+  }
   window.requestAnimationFrame( loop );
 }
