60fa0643d50c96307ded6faaa067a5c7303e75ad
beeboboboboe <125676148+beeboboboboe@users.noreply.github.com>
2025-12-16 17:15:26 +0000

    f

--- VRLesson4_3Classwork/VRLesson4_3Classwork/script.js
+++ VRLesson4_3Classwork/VRLesson4_3Classwork/script.js
@@ -1,9 +1,13 @@
 let rnd = (l,u) => Math.random()*(u-l)+l;
-let scene;
+let scene, ball, balls=[];
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   //Challenge 2: Create 200 Balls at random locations and watch them fall or push them off the end
-
-
+  for(let i=0;i<201;i++){
+    let x=rnd(-20, 20);
+    let z=rnd(-20, 20);
+    let b = new Ball(x, 1, z);
+    balls.push(b);
+  }
   
 })
